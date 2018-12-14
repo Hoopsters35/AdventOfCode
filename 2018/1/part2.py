720a5b80d0f2db00e0dbd7eaadb5a3fe0f3c17a1
Justin <justinknueppel@gmail.com>
2018-12-14 09:48:16 -0500

    solve problem 1 part 2

---
+++ 2018/1/part2.py
@@ -0,0 +1,14 @@
+with open ("day1inp.txt") as f:
+    lines = f.readlines()
+
+total = 0
+seen = set()
+dupfound = False
+while not dupfound:
+    for line in lines:
+        seen.add(total)
+        total = int(eval(f'{total}{line}'))
+        if total in seen:
+            dupfound = True
+            print(f'Duplicate found: {total}')
+            break
