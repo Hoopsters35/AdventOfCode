ead6bacef480b8d3069bc26cf3131b88e5b9c14e
Justin <justinknueppel@gmail.com>
2018-12-15 02:35:17 -0500

    work on part 2

--- 2018/02/part2.py
+++ 2018/02/part2.py
@@ -1,4 +1,26 @@
+def difby1(str1, str2):
+    errs = 0
+    for i, c in enumerate(str1):
+        if str2[i] != c:
+            errs += 1
+        if errs > 1:
+            return false
+    return True if errs == 1 else False
+
+def rembadchar(str1, str2):
+    newstr = ''
+    for i in range(len(str1)):
+        if str1[i] == str2[i]:
+            newstr += str1[i]
+    return newstr
+
 if __name__=='__main__':
     with open('day2inp.txt') as f:
         lines = f.readlines()
 
+    for line in lines:
+        for test in lines:
+            if difby1(line, test):
+                print(rembadchar(line, test))
+                return
+    print('None found')
