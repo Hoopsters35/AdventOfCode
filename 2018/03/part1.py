3dc6d7dbe9a2498d27b804730bebb853144ca326
Justin <justinknueppel@gmail.com>
2018-12-15 12:12:55 -0500

    solve part 1 of day 3

--- 2018/03/part1.py
+++ 2018/03/part1.py
@@ -1,18 +1,24 @@
-class Cut:
-    def __init__(self, num, fromL, fromT, wid, hei):
-        self.id = num
-        self.coords = (fromL, fromT)
-        self.dims = (wid, hei)
-
+def updategrid(grid, match):
+    for row in range(match[3]):
+        for col in range(match[4]):
+            grid[match[1] + row][match[2] + col] += 1
 
 if __name__=='__main__':
     with open('day3inp.txt') as f:
         lines = f.readlines()
 
     import re
+    from collections import Counter
+    import itertools
     regex = re.compile(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
-    line = lines[0]
-    match = regex.match(line)
-    matches = [regex.match(line).group() for line in lines]
-    print(max([group[1] + group[3] for group in matches]))
+    matches = [tuple(map(lambda x : int(x), regex.match(line).groups())) for line in lines]
 
+    n = 1000
+    grid = [[0 for i in range(n)] for j in range(n)]
+    for match in matches:
+        updategrid(grid, match)
+
+    c = Counter(itertools.chain.from_iterable(grid))
+    print(n*n - c[0] - c[1])
+
+# 110,546
