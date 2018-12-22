a309088105c70263bdb279d0a3c0e35c8846ca16
Justin <justinknueppel@gmail.com>
2018-12-22 11:06:23 -0500

    work on day 16, need to chang ehow the functions work

--- 2018/16/part1.py
+++ 2018/16/part1.py
@@ -9,7 +9,150 @@ class Sample:
 
 
 class Functions:
-    def addr(nums):
+    @staticmethod
+    def addr(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(nums[codes[1]] + nums[codes[2]])
+        return new_nums
+
+    @staticmethod
+    def addi(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(nums[codes[1]] + nums[2])
+        return new_nums
+
+    @staticmethod
+    def mulr(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(nums[codes[1]] * nums[codes[2]])
+        return new_nums
+
+    @staticmethod
+    def muli(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(nums[codes[1]] * nums[2])
+        return new_nums
+
+    @staticmethod
+    def banr(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(nums[codes[1]] & nums[codes[2]])
+        return new_nums
+
+    @staticmethod
+    def bani(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(nums[codes[1]] & nums[2])
+        return new_nums
+
+    @staticmethod
+    def borr(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(nums[codes[1]] | nums[codes[2]])
+        return new_nums
+
+    @staticmethod
+    def bori(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(nums[codes[1]] | nums[2])
+        return new_nums
+
+    @staticmethod
+    def setr(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(codes[nums[1]])
+        return new_nums
+
+    @staticmethod
+    def seti(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(nums[1])
+        return new_nums
+
+    @staticmethod
+    def gtir(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(1 if nums[1] > nums[codes[2]] else 0)
+        return new_nums
+
+    @staticmethod
+    def gtri(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(1 if nums[codes[1]] > nums[2] else 0)
+        return new_nums
+
+    @staticmethod
+    def gtrr(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(1 if nums[codes[1]] > nums[codes[2]] else 0)
+        return new_nums
+
+    @staticmethod
+    def eqir(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(1 if nums[1] == nums[codes[2]] else 0)
+        return new_nums
+
+    @staticmethod
+    def eqri(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(1 if nums[codes[1]] == nums[2] else 0)
+        return new_nums
+
+    @staticmethod
+    def eqrr(codes, nums):
+        new_nums = []
+        new_nums.append(nums[0])
+        new_nums.append(nums[1])
+        new_nums.append(nums[2])
+        new_nums.append(1 if nums[codes[1]] == nums[codes[2]] else 0)
+        return new_nums
+
 
 def process_input(lines):
     samples = []
@@ -24,9 +167,15 @@ def process_input(lines):
         samples.append(Sample(before, codes, after))
     return samples
 
+def exec_function(fun, codes, nums):
+    eval('print(nums)')
+    return eval(f'Functions.{fun}(codes, nums)')
+
 if __name__=='__main__':
     with open('day16inp.txt') as f:
         lines = [line.strip() for line in f.readlines()]
 
-    samples = process_input(lines)
-    print(samples[0])
+    #samples = process_input(lines)
+    #print(samples[0])
+    function_list = [func for func in dir(Functions) if callable(getattr(Functions, func)) and not '_' in func]
+    print(exec_function('addi', [9,2,1,2], [3, 2, 1, 1]))
