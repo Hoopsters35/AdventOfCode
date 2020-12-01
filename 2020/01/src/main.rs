cd7df14d0d346388010f596615680435a780b7bd
Justin Knueppel <jknuep828@cable.comcast.com>
2020-11-30 20:02:51 -0500

    Use find pair helper function

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -5,10 +5,9 @@ fn main() {
     let contents = fs::read_to_string("./data/part1.txt").expect("Failed to open file");
     let nums: Vec<i32> = contents.split("\n")
         .filter(|string| !string.is_empty())
-        .map(|string| string.parse::<i32>()
-        .unwrap())
+        .map(|string| string.parse::<i32>().unwrap())
         .collect();
-    
+
     let part1 = part1(&nums);
     println!("{}", part1);
     // 485739
@@ -18,29 +17,29 @@ fn main() {
     // 161109702
 }
 
-fn part1(nums: &Vec<i32>) -> i32 {
+fn find_pair(nums: &Vec<i32>, target: i32) -> (i32, i32) {
     let mut targets: HashSet<i32> = HashSet::new();
     for num in nums {
         if targets.contains(&num) {
-            return num * (2020 - num);
+            return (*num, (target - num));
         } else {
-            targets.insert(2020 - num);
+            targets.insert(target - num);
         }
     }
-    return -1;
+    return (-1, -1);
+}
+
+fn part1(nums: &Vec<i32>) -> i32 {
+    let (num1, num2) = find_pair(&nums, 2020);
+    return num1 * num2;
 }
 
 fn part2(nums: &Vec<i32>) -> i32 {
-    let single_vals: HashSet<i32> = nums.iter().cloned().collect();
-    for first_val in &single_vals {
+    for first_val in nums {
         let target = 2020 - first_val;
-        let mut targets: HashSet<i32> = HashSet::new();
-        for second_val in &single_vals {
-            if targets.contains(&second_val) {
-                return first_val * second_val * (2020 - first_val - second_val);
-            } else {
-                targets.insert(target - second_val);
-            }
+        let (second_val, third_val) = find_pair(nums, target);
+        if second_val != -1 {
+            return second_val * third_val * first_val;
         }
     }
     return -1;
