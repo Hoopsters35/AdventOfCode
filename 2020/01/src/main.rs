c9d2f827e5e44105ba00034ce64af42e5989983a
Justin Knueppel <jknuep828@cable.comcast.com>
2020-11-30 19:17:02 -0500

    Finish day 1 part 2

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -9,18 +9,38 @@ fn main() {
         .unwrap())
         .collect();
     
-    let part1 = part1(nums);
+    let part1 = part1(&nums);
     println!("{}", part1);
     // 485739
+
+    let part2 = part2(&nums);
+    println!("{}", part2);
+    // 161109702
 }
 
-fn part1(nums: Vec<i32>) -> i32 {
+fn part1(nums: &Vec<i32>) -> i32 {
     let mut targets: HashSet<i32> = HashSet::new();
     for num in nums {
         if targets.contains(&num) {
             return num * (2020 - num);
         } else {
             targets.insert(2020 - num);
+        }
+    }
+    return -1;
+}
+
+fn part2(nums: &Vec<i32>) -> i32 {
+    let single_vals: HashSet<i32> = nums.iter().cloned().collect();
+    for first_val in &single_vals {
+        let target = 2020 - first_val;
+        let mut targets: HashSet<i32> = HashSet::new();
+        for second_val in &single_vals {
+            if targets.contains(&second_val) {
+                return first_val * second_val * (2020 - first_val - second_val);
+            } else {
+                targets.insert(target - second_val);
+            }
         }
     }
     return -1;
