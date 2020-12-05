ae95fbf5f09eea5442af10c249ccf58c7f4ca56c
Justin Knueppel <justinknueppel@gmail.com>
2020-12-05 12:58:17 -0500

    Finish part 2

--- 2020/03/src/index.ts
+++ 2020/03/src/index.ts
@@ -3,36 +3,65 @@ import { promises as fs } from "fs";
 const TREE = "#";
 
 interface Slope {
-    x: number,
-    y: number
+  x: number;
+  y: number;
 }
 
 const main = async () => {
-    const content = await (await fs.readFile("data/map.txt")).toString();
-    const map = content.split("\n").filter(line => line.length > 0);
+  const content = await (await fs.readFile("data/map.txt")).toString();
+  const map = content.split("\n").filter((line) => line.length > 0);
 
-    const slope: Slope = {
-        x: 3,
-        y: 1
-    }
+  const slope: Slope = {
+    x: 3,
+    y: 1,
+  };
 
-    console.log(numTreesEncountered(map, slope));
-    // 178
-    
-}
+  console.log(numTreesEncountered(map, slope));
+  // 178
+
+  const slopes: Array<Slope> = [
+    {
+      x: 1,
+      y: 1,
+    },
+    {
+      x: 3,
+      y: 1,
+    },
+    {
+      x: 5,
+      y: 1,
+    },
+    {
+      x: 7,
+      y: 1,
+    },
+    {
+      x: 1,
+      y: 2,
+    },
+  ];
+
+  console.log(
+    slopes
+      .map((slope) => numTreesEncountered(map, slope))
+      .reduce((product, trees) => product * trees)
+  );
+  // 3492520200
+};
 
 const numTreesEncountered = (map: Array<string>, slope: Slope): number => {
-    let trees = 0;
-    let x = 0;
-    let y = 0;
-    while (y < map.length) {
-        if (map[y].charAt(x) == TREE) {
-            trees++;
-        }
-        x = (x + slope.x) % map[y].length;
-        y += slope.y;
+  let trees = 0;
+  let x = 0;
+  let y = 0;
+  while (y < map.length) {
+    if (map[y].charAt(x) == TREE) {
+      trees++;
     }
-    return trees;
-}
+    x = (x + slope.x) % map[y].length;
+    y += slope.y;
+  }
+  return trees;
+};
 
 main();
