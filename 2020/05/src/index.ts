7168534c5c23a82255958ce2be6a38f4930229c1
Justin Knueppel <justinknueppel@gmail.com>
2020-12-05 17:21:53 -0500

    Finish part 2

--- 2020/05/src/index.ts
+++ 2020/05/src/index.ts
@@ -21,6 +21,9 @@ const main = async () => {
 
   console.log(maxId(boardingPasses));
   // 818
+
+  console.log(openSeat(boardingPasses));
+  // 559
 };
 
 const rowToInt = (row: string) =>
@@ -29,13 +32,30 @@ const rowToInt = (row: string) =>
 const columnToInt = (column: string) =>
   parseInt(column.replace(/L/g, "0").replace(/R/g, "1"), 2);
 
+const boardingPassId = (boardingPass: BoardingPass): number => {
+  const row = rowToInt(boardingPass.row);
+  const column = columnToInt(boardingPass.column);
+  return row * 8 + column;
+};
+
 const maxId = (boardingPasses: Array<BoardingPass>): number => {
-  const ids = boardingPasses.map((boardingPass) => {
-    const row = rowToInt(boardingPass.row);
-    const column = columnToInt(boardingPass.column);
-    return row * 8 + column;
-  });
+  const ids = boardingPasses.map(boardingPassId);
   return Math.max(...ids);
+};
+
+const openSeat = (boardingPasses: Array<BoardingPass>): number => {
+  const ids = boardingPasses.map(boardingPassId);
+  const emptySeats: Array<number> = [];
+  for (let id = 0; id < 127 * 8 + 7; id++) {
+    if (!ids.includes(id)) {
+      emptySeats.push(id);
+    }
+  }
+  const seat = emptySeats.find(
+    (seatId) =>
+      !emptySeats.includes(seatId - 1) && !emptySeats.includes(seatId + 1)
+  );
+  return seat || -1;
 };
 
 main();
